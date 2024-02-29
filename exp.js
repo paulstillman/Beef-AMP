41cf6917ba0c6a775893d4ee86f4f0bef9da6d5a
Paul Stillman <paulstillman@users.noreply.github.com>
2024-02-28 16:02:06 -0800

    add target images

--- exp.js
+++ exp.js
@@ -4,9 +4,6 @@ const jsPsych = initJsPsych({
     jsPsych.data.displayData();
     // console.log(jsPsych.data.get().csv());
   },
-  extensions: [
-    { type: jsPsychExtensionMouseTracking, params: { minimum_sample_time: 0, events: ['mousemove', 'mouseleave'] } }
-  ]
 });
 
 function shuffleArray(array) {
@@ -23,30 +20,12 @@ if (sub_id === undefined) {
 }
 jsPsych.data.addProperties({ subject_id: sub_id });
 const fname = `${sub_id}.csv`;
-console.log(fname)
 
 
-let kface = jsPsych.data.getURLVariable('kface');
-let fface = jsPsych.data.getURLVariable('fface');
-let sface = jsPsych.data.getURLVariable('sface');
-if (kface === undefined) {
-  let face_array = [1,2,3,4,5]
-  shuffleArray(face_array)
-  kface = face_array[0];
-  fface = face_array[1];
-  sface = face_array[2];
-}
-kface = '<img src="./img/target_victim/Target' + kface + '.png">'
-fface = '<img src="./img/target_victim/Target' + fface + '.png">'
-sface = '<img src="./img/target_victim/Target' + sface + '.png">'
-
-console.log(kface)
-console.log(fface)
-console.log(sface)
 
 
 let timeline = [];
-
+/*
 var preload = {
   type: jsPsychPreload,
   images: ['./img/bad/bloodknife1.jpg',
@@ -68,10 +47,15 @@ var preload = {
     './img/target_victim/Target4.png',
     './img/target_victim/Target5.png']
 };
-
-
-
 timeline.push(preload)
+*/
+
+var preload = {
+  type: jsPsychPreload,
+  auto_preload: true
+}
+timeline.push(preload)
+
 
 const good_right = Math.floor(Math.random() * 2);
 if (good_right === 1) {
@@ -90,7 +74,7 @@ var enter_fullscreen = {
 // timeline.push(enter_fullscreen)
 
 
-/* instructions */
+/* 
 var instr_page1 = `<div class='instructions'>
 
 <p>For the next part of the study, we are interested in your evaluations of different things, 
@@ -123,214 +107,60 @@ instr_block1 = {
     pages: [instr_page1, instr_page2],
     show_clickable_nav: true,
 };
-
 timeline.push(instr_block1)
-
-const start_screen_practice = {
-  type: jsPsychHtmlButtonResponsePES,
-  stimulus: `Practice Trial: Press the START button below to begin`,
-  choices: ['START'],
-  data: {
-    task: 'start'
-  },
-  button_html: '<button class="jspsych-btn" style = "position:absolute; bottom: 0px; left: 50%; transform:translate(-50%); font-weight: bold">%choice%</button>',
-};
-
-const mt_trial_practice = {
-  type: jsPsychHtmlButtonResponsePES,
-  stimulus: '<img src="./img/good/flowers6.jpg">',
-  choices: choices_goodbad,
-  adjust_aspect_ratio: 0,
-  button_html: ['<button class="jspsych-btn mt-response-btn" id="left_response" style = "position:absolute; left: 0px; top: 0px">%choice%</button>', '<button class="jspsych-btn mt-response-btn" id="right_response" style = "position:absolute; right:0px; top: 0px">%choice%</button>'],
-  slowmouse_message: `Please begin moving your mouse<br>as soon as the image appears`,
-  mouseout_message: `Please keep your mouse<br>in the browser window`,
-  data: {
-    task: 'MT_practice',
-    good_right: good_right,
-    stim_type:  'practice'
-  },
-  extensions: [
-    { type: jsPsychExtensionMouseTracking }
-  ]
-};
-timeline.push(start_screen_practice)
-timeline.push(mt_trial_practice)
+*/
 
 
-var instr_page3 = `<div class='instructions'>
+targets = jsPsych.randomization.sampleWithoutReplacement(all_targets, 6)
+console.log(targets)
 
-<p>You will now do the evaluation task. In total, you will do 24 evaluations. Some items may be repeated.</p>
-
-<p>Again, please start moving your mouse as soon as the picture appears, 
-and don't move your mouse outside of the browser window.</p>
-
-<p> After the evaluation task, you will be redirected to the last part of the study.</p>
-
-<p>Press next to begin the evaluation task</p></div>`; 
-instr_block2 = {
-  type: jsPsychInstructions,
-  pages: [instr_page3],
-  show_clickable_nav: true,
-};
-
-timeline.push(instr_block2)
-
-
-const target_list = [
-  { stimulus: kface, stim_type: 'kevin' },
-  { stimulus: kface, stim_type: 'kevin' },
-  { stimulus: kface, stim_type: 'kevin' },
-  { stimulus: kface, stim_type: 'kevin' },
-  { stimulus: fface, stim_type: 'francis'  },
-  { stimulus: fface, stim_type: 'francis'  },
-  { stimulus: fface, stim_type: 'francis'  },
-  { stimulus: fface, stim_type: 'francis'  },
-  { stimulus: sface, stim_type: 'scott'  },
-  { stimulus: sface, stim_type: 'scott'  },
-  { stimulus: sface, stim_type: 'scott'  },
-  { stimulus: sface, stim_type: 'scott'  },
-  { stimulus: '<img src="./img/good/Cat5.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/good/Lake12.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/good/Rainbow2.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/good/flowers2.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/good/Lake2.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/good/Dog6.jpg">', stim_type: 'positive' },
-  { stimulus: '<img src="./img/bad/bloodknife1.jpg">', stim_type: 'negative' },
-  { stimulus: '<img src="./img/bad/caraccident2.jpg">', stim_type: 'negative' },
-  { stimulus: '<img src="./img/bad/feces2.jpg">', stim_type: 'negative' },
-  { stimulus: '<img src="./img/bad/fire9.jpg">', stim_type: 'negative' },
-  { stimulus: '<img src="./img/bad/planecrash2.jpg">', stim_type: 'negative' },
-  { stimulus: '<img src="./img/bad/shot3.jpg">', stim_type: 'negative' }
+const prime_list = [
+  { stimulus: './img/burgers/2patties_solo.jpeg', stim_type: 'beef_burger_1' },
+  { stimulus: './img/burgers/double-burger.jpeg', stim_type: 'beef_burger_2' },
+  { stimulus: './img/burgers/thickburger.jpeg', stim_type: 'beef_burger_3'},
+  { stimulus: './img/steaks/steak_1.jpeg', stim_type: 'beef_steak_1' },
+  { stimulus: './img/steaks/steak_3.jpeg', stim_type: 'beef_steak_2' },
+  { stimulus: './img/steaks/steak_8.jpeg', stim_type: 'beef_steak_3'}
 ];
+console.log(prime_list)
 
+var press_to_start = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:32px;">Press any key to start</div>',
+  choices: "ALL_KEYS",
+  trial_duration: null,
+};
 
-const mt_trial_competent = {
-  type: jsPsychHtmlButtonResponsePES,
-  // stimulus: '<img src="./img/' + jsPsych.timelineVariable('stimulus') + '.png">',
+timeline.push(press_to_start)
+
+var fixation = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:60px;">+</div>',
+  choices: "NO_KEYS",
+  trial_duration: 250,
+};
+
+var prime_screen = {
+  type: jsPsychImageKeyboardResponse,
   stimulus: jsPsych.timelineVariable('stimulus'),
-  choices: choices_goodbad,
-  adjust_aspect_ratio: 0,
-  button_html: ['<button class="jspsych-btn mt-response-btn" id="left_response" style = "position:absolute; left: 0px; top: 0px">%choice%</button>', '<button class="jspsych-btn mt-response-btn" id="right_response" style = "position:absolute; right:0px; top: 0px">%choice%</button>'],
-  slowmouse_message: `Please begin moving your mouse<br>as soon as the image appears`,
-  mouseout_message: `Please keep your mouse<br>in the browser window`,
+  stimulus_width: 300,
+  choices: "NO_KEYS",
+  trial_duration: 200, 
   data: {
-    task: 'MT',
-    good_right: good_right,
-    stim_type:  jsPsych.timelineVariable('stim_type')
-  },
-  extensions: [
-    { type: jsPsychExtensionMouseTracking }
-  ]
-};
-
-
-const start_screen = {
-  type: jsPsychHtmlButtonResponsePES,
-  stimulus: ``,
-  choices: ['START'],
-  data: {
-    task: 'start'
-  },
-  button_html: '<button class="jspsych-btn" style = "position:absolute; bottom: 0px; left: 50%; transform:translate(-50%); font-weight: bold">%choice%</button>',
-};
+    stim_type: jsPsych.timelineVariable('stim_type')
+  }
+}
 
 var test_procedure = {
-  timeline: [start_screen, mt_trial_competent],
-  timeline_variables: target_list,
+  timeline: [fixation, prime_screen],
+  timeline_variables: prime_list,
   randomize_order: true
 }
 
 timeline.push(test_procedure)
 
-// timeline.push(start_screen)
-// timeline.push(button_test)
-// timeline.push(start_screen)
-// timeline.push(button_test)
 
 /*
-const instructions = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-      <p>In this experiment, a circle will appear in the center 
-      of the screen.</p><p>If the circle is <strong>blue</strong>, 
-      press the letter F on the keyboard as fast as you can.</p>
-      <p>If the circle is <strong>orange</strong>, press the letter J 
-      as fast as you can.</p>
-      <div style='width: 700px;'>
-      <div style='float: left;'><img src='img/blue.png'></img>
-      <p class='small'><strong>Press the F key</strong></p></div>
-      <div style='float: right;'><img src='img/orange.png'></img>
-      <p class='small'><strong>Press the J key</strong></p></div>
-      </div>
-      <p>Press any key to begin.</p>
-    `,
-    post_trial_gap: 2000
-  };
-  
-  timeline.push(instructions);
-
-var test_stimuli = [
-    { stimulus: "img/blue.png", correct_response: 'f'},
-    { stimulus: "img/orange.png", correct_response: 'j'}
-  ];
-  
-
-
-  var fixation = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<div style="font-size:60px;">+</div>',
-    choices: "NO_KEYS",
-    trial_duration: function(){
-      return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
-    },
-    data: {
-      task: 'fixation'
-    }
-  }
-  
-  
-var test = {
-    type: jsPsychImageKeyboardResponse,
-    stimulus: jsPsych.timelineVariable('stimulus'),
-    choices: ['f', 'j'],
-    data: {
-      task: 'response',
-      correct_response: jsPsych.timelineVariable('correct_response')
-    },
-    on_finish: function(data){
-      data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
-    }
-  }
-  
-var test_procedure = {
-    timeline: [fixation, test],
-    timeline_variables: test_stimuli,
-    randomize_order: true,
-    repetitions: 5
-  };
-  
-
-
-timeline.push(test_procedure);
-
-var debrief_block = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: function() {
-
-    var trials = jsPsych.data.get().filter({task: 'response'});
-    var correct_trials = trials.filter({correct: true});
-    var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
-    var rt = Math.round(correct_trials.select('rt').mean());
-
-    return `<p>You responded correctly on ${accuracy}% of the trials.</p>
-      <p>Your average response time was ${rt}ms.</p>
-      <p>Press any key to complete the experiment. Thank you!</p>`;
-
-  }
-};
-timeline.push(debrief_block);
-*/
-
 var leave_fullscreen = {
   type: jsPsychFullscreen,
   fullscreen_mode: false,
@@ -338,8 +168,8 @@ var leave_fullscreen = {
 }
 
 timeline.push(leave_fullscreen)
-
-
+*/
+/*
 const save_data = {
   type: jsPsychPipe,
   action: "save",
@@ -348,9 +178,7 @@ const save_data = {
   data_string: () => jsPsych.data.get().csv()
 };
 timeline.push(save_data);
-
+*/
 
 jsPsych.run(timeline);
-
-// console.log(jsPsych.data.get().csv());
 
---
+++ targets.js
@@ -0,0 +1,602 @@
+var all_targets = [
+  {
+    "target_name": "./img/targets/pic1.png"
+  },
+  {
+    "target_name": "./img/targets/pic2.png"
+  },
+  {
+    "target_name": "./img/targets/pic3.png"
+  },
+  {
+    "target_name": "./img/targets/pic4.png"
+  },
+  {
+    "target_name": "./img/targets/pic5.png"
+  },
+  {
+    "target_name": "./img/targets/pic6.png"
+  },
+  {
+    "target_name": "./img/targets/pic7.png"
+  },
+  {
+    "target_name": "./img/targets/pic8.png"
+  },
+  {
+    "target_name": "./img/targets/pic9.png"
+  },
+  {
+    "target_name": "./img/targets/pic10.png"
+  },
+  {
+    "target_name": "./img/targets/pic11.png"
+  },
+  {
+    "target_name": "./img/targets/pic12.png"
+  },
+  {
+    "target_name": "./img/targets/pic13.png"
+  },
+  {
+    "target_name": "./img/targets/pic14.png"
+  },
+  {
+    "target_name": "./img/targets/pic15.png"
+  },
+  {
+    "target_name": "./img/targets/pic16.png"
+  },
+  {
+    "target_name": "./img/targets/pic17.png"
+  },
+  {
+    "target_name": "./img/targets/pic18.png"
+  },
+  {
+    "target_name": "./img/targets/pic19.png"
+  },
+  {
+    "target_name": "./img/targets/pic20.png"
+  },
+  {
+    "target_name": "./img/targets/pic21.png"
+  },
+  {
+    "target_name": "./img/targets/pic22.png"
+  },
+  {
+    "target_name": "./img/targets/pic23.png"
+  },
+  {
+    "target_name": "./img/targets/pic24.png"
+  },
+  {
+    "target_name": "./img/targets/pic25.png"
+  },
+  {
+    "target_name": "./img/targets/pic26.png"
+  },
+  {
+    "target_name": "./img/targets/pic27.png"
+  },
+  {
+    "target_name": "./img/targets/pic28.png"
+  },
+  {
+    "target_name": "./img/targets/pic29.png"
+  },
+  {
+    "target_name": "./img/targets/pic30.png"
+  },
+  {
+    "target_name": "./img/targets/pic31.png"
+  },
+  {
+    "target_name": "./img/targets/pic32.png"
+  },
+  {
+    "target_name": "./img/targets/pic33.png"
+  },
+  {
+    "target_name": "./img/targets/pic34.png"
+  },
+  {
+    "target_name": "./img/targets/pic35.png"
+  },
+  {
+    "target_name": "./img/targets/pic36.png"
+  },
+  {
+    "target_name": "./img/targets/pic37.png"
+  },
+  {
+    "target_name": "./img/targets/pic38.png"
+  },
+  {
+    "target_name": "./img/targets/pic39.png"
+  },
+  {
+    "target_name": "./img/targets/pic40.png"
+  },
+  {
+    "target_name": "./img/targets/pic41.png"
+  },
+  {
+    "target_name": "./img/targets/pic42.png"
+  },
+  {
+    "target_name": "./img/targets/pic43.png"
+  },
+  {
+    "target_name": "./img/targets/pic44.png"
+  },
+  {
+    "target_name": "./img/targets/pic45.png"
+  },
+  {
+    "target_name": "./img/targets/pic46.png"
+  },
+  {
+    "target_name": "./img/targets/pic47.png"
+  },
+  {
+    "target_name": "./img/targets/pic48.png"
+  },
+  {
+    "target_name": "./img/targets/pic49.png"
+  },
+  {
+    "target_name": "./img/targets/pic50.png"
+  },
+  {
+    "target_name": "./img/targets/pic51.png"
+  },
+  {
+    "target_name": "./img/targets/pic52.png"
+  },
+  {
+    "target_name": "./img/targets/pic53.png"
+  },
+  {
+    "target_name": "./img/targets/pic54.png"
+  },
+  {
+    "target_name": "./img/targets/pic55.png"
+  },
+  {
+    "target_name": "./img/targets/pic56.png"
+  },
+  {
+    "target_name": "./img/targets/pic57.png"
+  },
+  {
+    "target_name": "./img/targets/pic58.png"
+  },
+  {
+    "target_name": "./img/targets/pic59.png"
+  },
+  {
+    "target_name": "./img/targets/pic60.png"
+  },
+  {
+    "target_name": "./img/targets/pic61.png"
+  },
+  {
+    "target_name": "./img/targets/pic62.png"
+  },
+  {
+    "target_name": "./img/targets/pic63.png"
+  },
+  {
+    "target_name": "./img/targets/pic64.png"
+  },
+  {
+    "target_name": "./img/targets/pic65.png"
+  },
+  {
+    "target_name": "./img/targets/pic66.png"
+  },
+  {
+    "target_name": "./img/targets/pic67.png"
+  },
+  {
+    "target_name": "./img/targets/pic68.png"
+  },
+  {
+    "target_name": "./img/targets/pic69.png"
+  },
+  {
+    "target_name": "./img/targets/pic70.png"
+  },
+  {
+    "target_name": "./img/targets/pic71.png"
+  },
+  {
+    "target_name": "./img/targets/pic72.png"
+  },
+  {
+    "target_name": "./img/targets/pic73.png"
+  },
+  {
+    "target_name": "./img/targets/pic74.png"
+  },
+  {
+    "target_name": "./img/targets/pic75.png"
+  },
+  {
+    "target_name": "./img/targets/pic76.png"
+  },
+  {
+    "target_name": "./img/targets/pic77.png"
+  },
+  {
+    "target_name": "./img/targets/pic78.png"
+  },
+  {
+    "target_name": "./img/targets/pic79.png"
+  },
+  {
+    "target_name": "./img/targets/pic80.png"
+  },
+  {
+    "target_name": "./img/targets/pic81.png"
+  },
+  {
+    "target_name": "./img/targets/pic82.png"
+  },
+  {
+    "target_name": "./img/targets/pic83.png"
+  },
+  {
+    "target_name": "./img/targets/pic84.png"
+  },
+  {
+    "target_name": "./img/targets/pic85.png"
+  },
+  {
+    "target_name": "./img/targets/pic86.png"
+  },
+  {
+    "target_name": "./img/targets/pic87.png"
+  },
+  {
+    "target_name": "./img/targets/pic88.png"
+  },
+  {
+    "target_name": "./img/targets/pic89.png"
+  },
+  {
+    "target_name": "./img/targets/pic90.png"
+  },
+  {
+    "target_name": "./img/targets/pic91.png"
+  },
+  {
+    "target_name": "./img/targets/pic92.png"
+  },
+  {
+    "target_name": "./img/targets/pic93.png"
+  },
+  {
+    "target_name": "./img/targets/pic94.png"
+  },
+  {
+    "target_name": "./img/targets/pic95.png"
+  },
+  {
+    "target_name": "./img/targets/pic96.png"
+  },
+  {
+    "target_name": "./img/targets/pic97.png"
+  },
+  {
+    "target_name": "./img/targets/pic98.png"
+  },
+  {
+    "target_name": "./img/targets/pic99.png"
+  },
+  {
+    "target_name": "./img/targets/pic100.png"
+  },
+  {
+    "target_name": "./img/targets/pic101.png"
+  },
+  {
+    "target_name": "./img/targets/pic102.png"
+  },
+  {
+    "target_name": "./img/targets/pic103.png"
+  },
+  {
+    "target_name": "./img/targets/pic104.png"
+  },
+  {
+    "target_name": "./img/targets/pic105.png"
+  },
+  {
+    "target_name": "./img/targets/pic106.png"
+  },
+  {
+    "target_name": "./img/targets/pic107.png"
+  },
+  {
+    "target_name": "./img/targets/pic108.png"
+  },
+  {
+    "target_name": "./img/targets/pic109.png"
+  },
+  {
+    "target_name": "./img/targets/pic110.png"
+  },
+  {
+    "target_name": "./img/targets/pic111.png"
+  },
+  {
+    "target_name": "./img/targets/pic112.png"
+  },
+  {
+    "target_name": "./img/targets/pic113.png"
+  },
+  {
+    "target_name": "./img/targets/pic114.png"
+  },
+  {
+    "target_name": "./img/targets/pic115.png"
+  },
+  {
+    "target_name": "./img/targets/pic116.png"
+  },
+  {
+    "target_name": "./img/targets/pic117.png"
+  },
+  {
+    "target_name": "./img/targets/pic118.png"
+  },
+  {
+    "target_name": "./img/targets/pic119.png"
+  },
+  {
+    "target_name": "./img/targets/pic120.png"
+  },
+  {
+    "target_name": "./img/targets/pic121.png"
+  },
+  {
+    "target_name": "./img/targets/pic122.png"
+  },
+  {
+    "target_name": "./img/targets/pic123.png"
+  },
+  {
+    "target_name": "./img/targets/pic124.png"
+  },
+  {
+    "target_name": "./img/targets/pic125.png"
+  },
+  {
+    "target_name": "./img/targets/pic126.png"
+  },
+  {
+    "target_name": "./img/targets/pic127.png"
+  },
+  {
+    "target_name": "./img/targets/pic128.png"
+  },
+  {
+    "target_name": "./img/targets/pic129.png"
+  },
+  {
+    "target_name": "./img/targets/pic130.png"
+  },
+  {
+    "target_name": "./img/targets/pic131.png"
+  },
+  {
+    "target_name": "./img/targets/pic132.png"
+  },
+  {
+    "target_name": "./img/targets/pic133.png"
+  },
+  {
+    "target_name": "./img/targets/pic134.png"
+  },
+  {
+    "target_name": "./img/targets/pic135.png"
+  },
+  {
+    "target_name": "./img/targets/pic136.png"
+  },
+  {
+    "target_name": "./img/targets/pic137.png"
+  },
+  {
+    "target_name": "./img/targets/pic138.png"
+  },
+  {
+    "target_name": "./img/targets/pic139.png"
+  },
+  {
+    "target_name": "./img/targets/pic140.png"
+  },
+  {
+    "target_name": "./img/targets/pic141.png"
+  },
+  {
+    "target_name": "./img/targets/pic142.png"
+  },
+  {
+    "target_name": "./img/targets/pic143.png"
+  },
+  {
+    "target_name": "./img/targets/pic144.png"
+  },
+  {
+    "target_name": "./img/targets/pic145.png"
+  },
+  {
+    "target_name": "./img/targets/pic146.png"
+  },
+  {
+    "target_name": "./img/targets/pic147.png"
+  },
+  {
+    "target_name": "./img/targets/pic148.png"
+  },
+  {
+    "target_name": "./img/targets/pic149.png"
+  },
+  {
+    "target_name": "./img/targets/pic150.png"
+  },
+  {
+    "target_name": "./img/targets/pic151.png"
+  },
+  {
+    "target_name": "./img/targets/pic152.png"
+  },
+  {
+    "target_name": "./img/targets/pic153.png"
+  },
+  {
+    "target_name": "./img/targets/pic154.png"
+  },
+  {
+    "target_name": "./img/targets/pic155.png"
+  },
+  {
+    "target_name": "./img/targets/pic156.png"
+  },
+  {
+    "target_name": "./img/targets/pic157.png"
+  },
+  {
+    "target_name": "./img/targets/pic158.png"
+  },
+  {
+    "target_name": "./img/targets/pic159.png"
+  },
+  {
+    "target_name": "./img/targets/pic160.png"
+  },
+  {
+    "target_name": "./img/targets/pic161.png"
+  },
+  {
+    "target_name": "./img/targets/pic162.png"
+  },
+  {
+    "target_name": "./img/targets/pic163.png"
+  },
+  {
+    "target_name": "./img/targets/pic164.png"
+  },
+  {
+    "target_name": "./img/targets/pic165.png"
+  },
+  {
+    "target_name": "./img/targets/pic166.png"
+  },
+  {
+    "target_name": "./img/targets/pic167.png"
+  },
+  {
+    "target_name": "./img/targets/pic168.png"
+  },
+  {
+    "target_name": "./img/targets/pic169.png"
+  },
+  {
+    "target_name": "./img/targets/pic170.png"
+  },
+  {
+    "target_name": "./img/targets/pic171.png"
+  },
+  {
+    "target_name": "./img/targets/pic172.png"
+  },
+  {
+    "target_name": "./img/targets/pic173.png"
+  },
+  {
+    "target_name": "./img/targets/pic174.png"
+  },
+  {
+    "target_name": "./img/targets/pic175.png"
+  },
+  {
+    "target_name": "./img/targets/pic176.png"
+  },
+  {
+    "target_name": "./img/targets/pic177.png"
+  },
+  {
+    "target_name": "./img/targets/pic178.png"
+  },
+  {
+    "target_name": "./img/targets/pic179.png"
+  },
+  {
+    "target_name": "./img/targets/pic180.png"
+  },
+  {
+    "target_name": "./img/targets/pic181.png"
+  },
+  {
+    "target_name": "./img/targets/pic182.png"
+  },
+  {
+    "target_name": "./img/targets/pic183.png"
+  },
+  {
+    "target_name": "./img/targets/pic184.png"
+  },
+  {
+    "target_name": "./img/targets/pic185.png"
+  },
+  {
+    "target_name": "./img/targets/pic186.png"
+  },
+  {
+    "target_name": "./img/targets/pic187.png"
+  },
+  {
+    "target_name": "./img/targets/pic188.png"
+  },
+  {
+    "target_name": "./img/targets/pic189.png"
+  },
+  {
+    "target_name": "./img/targets/pic190.png"
+  },
+  {
+    "target_name": "./img/targets/pic191.png"
+  },
+  {
+    "target_name": "./img/targets/pic192.png"
+  },
+  {
+    "target_name": "./img/targets/pic193.png"
+  },
+  {
+    "target_name": "./img/targets/pic194.png"
+  },
+  {
+    "target_name": "./img/targets/pic195.png"
+  },
+  {
+    "target_name": "./img/targets/pic196.png"
+  },
+  {
+    "target_name": "./img/targets/pic197.png"
+  },
+  {
+    "target_name": "./img/targets/pic198.png"
+  },
+  {
+    "target_name": "./img/targets/pic199.png"
+  },
+  {
+    "target_name": "./img/targets/pic200.png"
+  }
+]
